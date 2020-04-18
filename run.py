e05336eb11aeb01be09d2c6feca4665509cd5fda
Ubuntu <ubuntu@ip-172-31-33-6.ec2.internal>
2020-04-18 13:19:11 +0000

    Added toggle important functionality

--- run.py
+++ run.py
@@ -3,20 +3,30 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-if os.path.exists("env.py"):
-    import env
 
 app = Flask(__name__)
 
+def import_mongouri():
+    if os.path.exists("env.py"):
+        import env
+        app.config["MONGO_URI"] = env.MONGO_URI
+    else:
+        app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+import_mongouri()
+
 app.config["MONGO_DBNAME"] = 'story_database'
-app.config["MONGO_URI"] = env.MONGO_URI
 
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/nav_home')
+@app.route('/nav_home', methods=['POST'])
 def nav_home():
     return render_template("index.html", elements=mongo.db.story_elements.find())
 
+@app.route('/important_home', methods=['POST'])
+def important_home():
+    return render_template("indeximportant.html", elements=mongo.db.story_elements.find( {'important': True} ))
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
