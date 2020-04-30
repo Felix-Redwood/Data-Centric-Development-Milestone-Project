0efd6b47a9eab8bb408bc664085f18981e3262c5
Ubuntu <ubuntu@ip-172-31-33-6.ec2.internal>
2020-04-30 11:29:46 +0000

    commented and formatted run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+# This file contains the functions that render the templates in the 'templates'
+# directory. These functions also pass through values, such as 'elements' or
+# 'categories' that help to render elements in each page.
+
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
@@ -5,62 +9,91 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
 def import_mongouri():
+    """If there is an env.py file containing the MONGO_URI, then the MONGO_URI
+    will be provided from that. If not, the program will search the local
+    environment for the MONGO_URI."""
     if os.path.exists("env.py"):
         import env
         app.config["MONGO_URI"] = env.MONGO_URI
     else:
         app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
+
 import_mongouri()
+
 
 app.config["MONGO_DBNAME"] = 'story_database'
 
+
 mongo = PyMongo(app)
+
+
+"""Error Handler - Redirects to 404.html upon a 404 error."""
 
 @app.errorhandler(404)
 def page_not_found(e):
     print("error 404, page not found")
     return render_template('404.html'), 404
 
+
+"""Homepage."""
+
 @app.route('/')
 @app.route('/nav_home', methods=['POST', 'GET'])
 def nav_home():
-    return render_template("index.html", elements=mongo.db.story_elements.find().limit(10))
+    return render_template("index.html",
+                            elements=mongo.db.story_elements.find().limit(10))
+
 
 @app.route('/important_home', methods=['POST'])
 def important_home():
-    return render_template("indeximportant.html", elements=mongo.db.story_elements.find( {'important': True} ).limit(10))
+    return render_template("indeximportant.html",
+                            elements=mongo.db.story_elements.find({'important': True}).limit(10))
+
 
 @app.route('/toggle_element_important_home/<element_id>', methods=['POST'])
 def toggle_element_important_home(element_id):
     print(element_id)
     importantbool = mongo.db.story_elements.find_one({'_id': ObjectId(element_id)})
-    print(importantbool)
-    if importantbool['important'] == True:
+    """The importantbool variable contains the 'important' status of the element,
+    which is a boolean. When a user switches an element's 'important' status,
+    this function changes that boolean in the MongoDB database and reloads
+    the page."""
+    if importantbool['important'] is True:
         mongo.db.story_elements.update_one(
-       {'_id': ObjectId(element_id)},
-       {'$set': { "important" : False }}
+       {'_id':ObjectId(element_id)},
+       {'$set':{"important":False}}
         )
     else:
         mongo.db.story_elements.update_one(
-       {'_id': ObjectId(element_id)},
-       {'$set': { "important" : True }}
+       {'_id':ObjectId(element_id)},
+       {'$set':{"important":True}}
         )
-    return render_template("index.html", elements=mongo.db.story_elements.find().limit(10))
+    return render_template("index.html",
+                            elements=mongo.db.story_elements.find().limit(10))
+
 
 @app.route('/delete_element_home/<element_id>')
 def delete_element_home(element_id):
     mongo.db.story_elements.remove({'_id': ObjectId(element_id)})
     return redirect(url_for('nav_home'))
 
+
+"""Elements Page."""
+
 @app.route('/nav_elements', methods=['POST', 'GET'])
 def nav_elements():
-    return render_template("elements.html", elements=mongo.db.story_elements.find())
+    return render_template("element/elements.html",
+                            elements=mongo.db.story_elements.find())
+
 
 @app.route('/important_elements', methods=['POST'])
 def important_elements():
-    return render_template("importantelements.html", elements=mongo.db.story_elements.find( {'important': True} ))
+    return render_template("element/important.html",
+                            elements=mongo.db.story_elements.find( {'important': True} ))
+
 
 @app.route('/toggle_element_important_elements/<element_id>', methods=['POST'])
 def toggle_element_important_elements(element_id):
@@ -75,23 +108,31 @@ def toggle_element_important_elements(element_id):
        {'_id': ObjectId(element_id)},
        {'$set': { "important" : True }}
         )
-    return render_template("elements.html", elements=mongo.db.story_elements.find())
+    return render_template("element/elements.html", elements=mongo.db.story_elements.find())
+
 
 @app.route('/edit_element/<element_id>')
 def edit_element(element_id):
     selected_element = mongo.db.story_elements.find_one({"_id": ObjectId(element_id)})
     all_categories = mongo.db.categories.find()
     all_subcategories = mongo.db.subcategories.find()
-    return render_template("editelement.html", element=selected_element, categories=all_categories, subcategories=all_subcategories)
+    return render_template("element/edit.html",
+                            element=selected_element,
+                            categories=all_categories,
+                            subcategories=all_subcategories)
+
 
 @app.route('/update_element/<element_id>', methods=['POST'])
 def update_element(element_id):
+    """As 'important' is a boolean, this 'if' function checks whether the
+    'important' toggle is on or not, and sets the value of a variable named
+    'important' accordingly."""
     if request.form.get('important') == 'on':
         important = True
     else:
         important = False
     elements = mongo.db.story_elements
-    elements.update( {'_id': ObjectId(element_id)},
+    elements.update({'_id': ObjectId(element_id)},
     {
      'category_name':request.form.get('category_name'),
      'subcategory':request.form.get('subcategory'),
@@ -104,24 +145,37 @@ def update_element(element_id):
     })
     return redirect(url_for('nav_elements'))
 
+
 @app.route('/insert_element', methods=['POST'])
 def insert_element():
     elements = mongo.db.story_elements
+    """Here we use to_dict, to ensure that the forms contents are in a format
+    that MongoDB can read."""
     elements.insert_one(request.form.to_dict())
     return redirect(url_for('nav_elements'))
 
+
 @app.route('/add_element')
 def add_element():
-    return render_template('addelement.html', categories=list(mongo.db.categories.find()), subcategories=list(mongo.db.subcategories.find()))
+    return render_template('element/add.html',
+                            categories=list(mongo.db.categories.find()),
+                            subcategories=list(mongo.db.subcategories.find()))
+
 
 @app.route('/delete_element/<element_id>')
 def delete_element(element_id):
     mongo.db.story_elements.remove({'_id': ObjectId(element_id)})
     return redirect(url_for('nav_elements'))
 
+
+"""Categories Page."""
+
 @app.route('/nav_categories', methods=['POST', 'GET'])
 def nav_categories():
-    return render_template("categories.html", categories=list(mongo.db.categories.find()), subcategories=list(mongo.db.subcategories.find()))
+    return render_template("category/categories.html",
+                            categories=list(mongo.db.categories.find()),
+                            subcategories=list(mongo.db.subcategories.find()))
+
 
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
@@ -129,14 +183,17 @@ def insert_category():
     categories.insert_one(request.form.to_dict())
     return redirect(url_for('nav_categories'))
 
+
 @app.route('/add_category')
 def add_category():
-    return render_template('addcategory.html')
+    return render_template('category/add.html')
+
 
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     selected_category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
-    return render_template("editcategory.html", category=selected_category)
+    return render_template("category/edit.html", category=selected_category)
+
 
 @app.route('/update_category/<category_id>', methods=['POST'])
 def update_category(category_id):
@@ -148,14 +205,20 @@ def update_category(category_id):
     })
     return redirect(url_for('nav_categories'))
 
+
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('nav_categories'))
 
+
+"""Subcategories Page."""
+
 @app.route('/nav_subcategories', methods=['POST', 'GET'])
 def nav_subcategories():
-    return render_template("subcategories.html", subcategories=mongo.db.subcategories.find())
+    return render_template("subcategory/subcategories.html",
+                            subcategories=mongo.db.subcategories.find())
+
 
 @app.route('/insert_subcategory', methods=['POST'])
 def insert_subcategory():
@@ -163,15 +226,21 @@ def insert_subcategory():
     subcategories.insert_one(request.form.to_dict())
     return redirect(url_for('nav_subcategories'))
 
+
 @app.route('/add_subcategory')
 def add_subcategory():
-    return render_template('addsubcategory.html', categories=mongo.db.categories.find())
+    return render_template('subcategory/add.html',
+                            categories=mongo.db.categories.find())
+
 
 @app.route('/edit_subcategory/<subcategory_id>')
 def edit_subcategory(subcategory_id):
     selected_subcategory = mongo.db.subcategories.find_one({"_id": ObjectId(subcategory_id)})
     all_categories = mongo.db.categories.find()
-    return render_template("editsubcategory.html", subcategory=selected_subcategory, categories=all_categories)
+    return render_template("subcategory/edit.html",
+                            subcategory=selected_subcategory,
+                            categories=all_categories)
+
 
 @app.route('/update_subcategory/<subcategory_id>', methods=['POST'])
 def update_subcategory(subcategory_id):
@@ -184,19 +253,30 @@ def update_subcategory(subcategory_id):
     })
     return redirect(url_for('nav_subcategories'))
 
+
 @app.route('/delete_subcategory/<subcategory_id>')
 def delete_subcategory(subcategory_id):
     mongo.db.subcategories.remove({'_id': ObjectId(subcategory_id)})
     return redirect(url_for('nav_subcategories'))
 
+
+"""Search Results Page."""
+
 @app.route('/search_results', methods=['POST', 'GET'])
 def search_results():
-#    search_inquiry = request.form.get('search_inquiry')
+    """This function queries the story_database text indexes in MongoDB. It
+    takes the search_inquiry, i.e the text sent through the search form and
+    searches through the text in each index."""
     categories_results = mongo.db.categories.find({'$text': {'$search':request.form.get('search_inquiry')}})
     subcategories_results = mongo.db.subcategories.find({'$text': {'$search':request.form.get('search_inquiry')}})
     elements_results = mongo.db.story_elements.find({'$text': {'$search':request.form.get('search_inquiry')}})
-    return render_template("searchresults.html", categories=categories_results, subcategories=subcategories_results, elements=elements_results)
+    return render_template("searchresults.html",
+                        categories=list(categories_results),
+                        subcategories=list(subcategories_results),
+                        elements=list(elements_results))
+
+
+"""Miscellaneous"""
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
-
